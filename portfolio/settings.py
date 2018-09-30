3f2628fcebdd467ce359631f1cd4a1e967286c28
Pritom-Mazhi <samiulsalehinpritom@gmail.com>
2018-09-30 14:40:16 +0600

    jobs app running 3rd commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #custom apps
+    #'blog',
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliopgdb',
+        'USER': 'pritom',
+        'PASSWORD': 'goponpgsqlpass',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -118,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
